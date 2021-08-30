b690ba54196a2af3d55b52f3975892430a7517b6
amr elgendy <amr86elgendy@gmail.com>
2021-08-30 21:27:39 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import ReactDOM from 'react-dom';
+import './index.css';
+import ApolloProvider from './ApolloProvider';
+import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(
+  ApolloProvider,
+  document.getElementById('root')
+);
+
+reportWebVitals();
